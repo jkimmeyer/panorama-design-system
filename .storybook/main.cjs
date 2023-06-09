9f18d3ed6d3e3168cab8c95a32fd72838c018c27
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2023-06-09 11:59:40 +0200

    Run update for storybook v7

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,11 +1,14 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  framework: "@storybook/web-components",
-  core: {
-    builder: "@storybook/builder-vite",
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/web-components-vite",
+    options: {}
   },
   features: {
-    storyStoreV7: true,
+    storyStoreV7: true
   },
+  docs: {
+    autodocs: true
+  }
 };
