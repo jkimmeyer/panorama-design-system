c41e56e5a415e02c6295ac1bd579bb9b44692e38
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2023-10-19 20:57:38 +0200

    Remove index file

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import { global as globalThis } from "@storybook/global";
-
-import { ButtonTag } from "./Button/Button";
-
-globalThis.Components = {
-  Button: ButtonTag,
-};
-
-globalThis.storybookRenderer = "web-components";
