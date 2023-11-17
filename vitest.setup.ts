37f93b325edabfa9e35be8991da7ab6497229750
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2023-11-17 12:59:59 +0100

    Add testing files

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,9 @@
 import { afterEach } from "vitest";
 import "@testing-library/jest-dom/vitest";
 import { fixtureCleanup } from "@open-wc/testing";
-import "vitest-axe/extend-expect";
+import { toHaveNoViolations } from "jest-axe";
+import { expect } from "vitest";
+expect.extend(toHaveNoViolations);
 
 // cleanup fixtures after each test
 afterEach(() => {
