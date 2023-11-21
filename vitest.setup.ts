a5e86f292f6ee86fb796a7d912c89f142089c786
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2023-11-21 21:22:20 +0100

    Add accessibility testing

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,11 +1,19 @@
 import { afterEach } from "vitest";
 import "@testing-library/jest-dom/vitest";
 import { fixtureCleanup } from "@open-wc/testing";
-import { toHaveNoViolations } from "jest-axe";
+import * as matchers from "vitest-axe/matchers";
+import { configureAxe } from "vitest-axe";
 import { expect } from "vitest";
-expect.extend(toHaveNoViolations);
+expect.extend(matchers);
+
+configureAxe({
+  rules: {
+    // disable landmark rules when testing isolated components.
+    region: { enabled: false },
+  },
+});
 
 // cleanup fixtures after each test
-afterEach(() => {
-  fixtureCleanup();
-});
+// afterEach(() => {
+//   fixtureCleanup();
+// });
