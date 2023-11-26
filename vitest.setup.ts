e1cbf295ff4e5b69121c1b182fce5401fb1e2e7d
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2023-11-26 17:27:11 +0100

    Remove superfluous packages

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,19 +1,8 @@
 import { afterEach } from "vitest";
 import "@testing-library/jest-dom/vitest";
 import { fixtureCleanup } from "@open-wc/testing";
-import * as matchers from "vitest-axe/matchers";
-import { configureAxe } from "vitest-axe";
-import { expect } from "vitest";
-expect.extend(matchers);
-
-configureAxe({
-  rules: {
-    // disable landmark rules when testing isolated components.
-    region: { enabled: false },
-  },
-});
 
 // cleanup fixtures after each test
-// afterEach(() => {
-//   fixtureCleanup();
-// });
+afterEach(() => {
+  fixtureCleanup();
+});
